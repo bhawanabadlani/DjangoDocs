8f118a6f2b383ead15ac9991d16eea4c2d2d8b57
bhawanabadlani <bhawanabadlani@gmail.com>
2019-11-26 15:52:29 +0530

    Activating Models

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
-def Question(models.Model):
-    question_text = models.Charfield(max_length=200)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-def Choice(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.Charfield(max_length=0)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-# Create your models here.
